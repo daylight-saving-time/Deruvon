764d5d7d5601c8f0087bc3a7eb98ff68d60cf1b7
Bass-Ocelot <selfishocelot@gmail.com>
2018-11-13 23:48:39 -0500

    WIP: Added jump and gravity logic

--- Player Movement Testing/KinematicPlayerMover.gd
+++ Player Movement Testing/KinematicPlayerMover.gd
@@ -13,7 +13,8 @@ var motion = Vector3()
 #var normal
 
 # Movement Constants
-const gravity = Vector3(0,-4.8,0)
+const gravity = Vector3(0,-9,0)
+const vertical_powah = gravity
 #const jump_deacceleration = 3 
 
 func _ready():
@@ -24,8 +25,8 @@ func _ready():
 	
 	
 func _physics_process(delta):
-	motion.x=0
-	motion.z=0
+#	motion.x=0
+#	motion.z=0
 	
 	
 	# INPUT KEYS
@@ -39,40 +40,45 @@ func _physics_process(delta):
 	
 	jump = Input.is_key_pressed(KEY_SPACE)
 	
-	#print(is_on_floor())
+	if is_on_floor():
+		motion.x=0
+		motion.z=0
+		vertical_powah= gravity
 	# Move forwards
-	if forward:
-		print("going forwards")
-		motion.x = speed
+		if forward:
+			print("going forwards")
+			motion.x = speed
 		
 	# Move left
-	if left:
-		print("going left")
-		motion.z = -speed
+		if left:
+			print("going left")
+			motion.z = -speed
 	
 	# Move right
-	if right:
-		print("going right")
-		motion.z = speed
+		if right:
+			print("going right")
+			motion.z = speed
 		
 	# Move backwards
-	if back:
-		print("going back")
-		motion.x = -speed
+		if back:
+			print("going back")
+			motion.x = -speed
+			
 		
 	#JUMPING WIP
 	if jump and is_on_floor():
-		
-#		print("jump")
-#		motion = move_and_slide(motion-gravity,Vector3(0,1,0))
-		pass
+		vertical_powah = Vector3(0,2,0)
+#		motion.y=jump_powah
+#		motion = move_and_slide(motion,Vector3(0,1,0))
 		
 	# Gravity
-#	if !is_on_floor():
+	if !is_on_floor():
 		#motion += gravity * delta
+		vertical_powah +=  gravity * delta
 
 	
-	motion = move_and_slide(motion+gravity,Vector3(0,1,0))
+	motion = move_and_slide(motion + vertical_powah,Vector3(0,1,0))
+	print (self.translation)
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
